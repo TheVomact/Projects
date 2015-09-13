cc749521ff2d518fa1a879152912222ef4501ead
TheVomact <unknown.abc@gmail.com>
2015-09-13 09:18:11 +0200

    reverse_a_string: Read exactly one line.

--- reverse_a_string/reverse.go
+++ reverse_a_string/reverse.go
@@ -4,18 +4,16 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
 	var reversed, str string
-	string_slice := make([]string, 3)
-	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Println("Enter a string.")
-	for scanner.Scan() {
-		string_slice = append(string_slice, scanner.Text())
-	}
-	str = strings.Join(string_slice, " ")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	str = scanner.Text()
+
 	for _, ch := range str {
 		reversed = string(ch) + reversed
 	}
